014b8c7cefc13f07dcaee9fcb6b3bd9279233be6
T-Dogg <47765355+T-Dogg-PO@users.noreply.github.com>
2021-10-27 07:28:32 +0200

    Fixed styling of Show More button

--- js/portfolio-scripts.js
+++ js/portfolio-scripts.js
@@ -3,11 +3,11 @@ function showHideDescription(project) {
     let moreText = document.querySelector(`.grid__item[project-name="${project}"] .more`);
     let btnText = document.querySelector(`.grid__item[project-name="${project}"] .showHideButton`);
   
-    if (btnText.innerHTML === "Show Description") {
-      btnText.innerHTML = "Hide Description"; 
+    if (btnText.innerHTML === "Read more") {
+      btnText.innerHTML = "Show less"; 
       moreText.style.display = "inline";
     } else {
-      btnText.innerHTML = "Show Description"; 
+      btnText.innerHTML = "Read more"; 
       moreText.style.display = "none";
     }
   }
